f742cd8dcd2733e77db0ae4dda5e34df635280ca
chenzhg33 <121579293@qq.com>
2013-09-30 09:19:04 +0800

    check

--- test.cpp
+++ test.cpp
@@ -3,58 +3,60 @@
 #include <stack>
 #include <set>
 #include <cstdlib>
-
 using namespace std;
-
-long long mi(int m, int n) {
-  long long sum = 1;
-  for (int i = 0; i < n; i++)
-    sum *= m;
-  return sum;
+bool isSort(int a[], int n) {
+  for (int i = 0; i < n - 1; i++)
+    if (a[i] > a[i+1])
+      return false;
+  return true;
 }
-int Sum(int a[], int n) {
-  int sum = 0;
-  for (int i = 0; i < n; i++)
-    sum += a[i];
-  return sum;
-}
-
-void sort(int a[], int n) {
-  for (int i = 0; i < n; i++)
-  for (int j = 0; j < n - 1; j++)
-    if (a[j] < a[j + 1]) {
+int bubleSort(int a[], int index) {
+  int flag = 1;
+  for (int i = 0; i < index - 1; i++)
+  for (int j = 0; j < index - 1 - i; j++) {
+    if (a[j] > a[j+1]) {
+      flag = 0;
       int temp = a[j];
       a[j] = a[j+1];
       a[j+1] = temp;
     }
+  }
+  return flag;
 }
-
+void electSort(int a[], int n) {
+for (int i = 0; i < n - 1; i++) {
+  int min = i;
+  for (int j = i + 1; j < n; j++) {
+    if (a[min] > a[j]) {
+      min = j;
+      }
+    }
+  int temp = a[i];
+  a[i] = a[min];
+  a[min] = temp;
+}
+}
+                        
+void print(int a[], int n) {
+  cout << a[0];
+  for (int i = 1; i < n; i++)
+    cout << ' ' << a[i];
+  cout << endl;
+}
+   
 int main() {
-  unsigned N;
-  cin >> N;
-  while(N--) {
-    int m = 0, n = 0;
-    cin >> m >> n;
-    set<string> ss;
-    long long sum = mi(m+1, n);
-    for (long long i = 1; i <= sum; i++) {
-      int a[10] = {0};
-      long long temp = i;
-      for (int j = 0; j < n; j++) {
-        a[j] = temp % (m+1);
-        temp /= (m+1);
-      }
-      if (Sum(a, n) == m) {
-        sort(a, n);    
-        string s;  
-        for (int i = 0; i < n; i++)
-          s += (a[i] + '0');
-        ss.insert(s);
-      }
-    }
-    cout << ss.size() << endl;
-    
-    }
+  int n;
+  cin >> n;
+  int a[500] = {0};
+  for (int i = 0; i < n; i++)
+    cin >> a[i];
+  sort(a,n);
+  print(a,n);
+  if (n%2 == 0) {
+    cout << (a[n/2 - 1] + a[n/2]) / 2 << endl;
+  } else {
+    cout << a[n/2] << endl;
+  }
   system("pause");
   return 0;
 }
